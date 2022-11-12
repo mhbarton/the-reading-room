2cf7aed66c0059a459430d9f3fa48df0c29084ff
Marianne Barton <mhood24@gmail.com>
2022-11-12 12:37:52 -0700

    CSS changes to components

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Card.css';
 import { Link } from 'react-router-dom';
 
-const Card = ({ image, title, author, type, id }) => {
+const Card = ({ image, title, author, id }) => {
     return (
         <Link to={`/${id}`} className= 'card' aria-label={title}>
             <div className='cover-image-container'>
